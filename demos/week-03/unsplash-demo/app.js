9afb816c3a097ead3b7c60990845ace5ef2206fd
Tim Smith <tim.smith2814@gmail.com>
2023-12-14 13:55:46 +0000

    Add unsplash demo

---
+++ demos/week-03/unsplash-demo/app.js
@@ -0,0 +1,45 @@
+// make our API call to unsplash and get images
+async function getImages(query) {
+  // make a fetch call to unsplash
+  const response = await fetch(
+    `https://api.unsplash.com/search/photos?query=${query}&client_id=ml6bRDnz2_FsW11E0KfUf9fJ6_Kw0vR2s9laeDaTvIc`
+  );
+
+  // turn my response into JSON
+  const json = await response.json();
+  console.log(json);
+  // return the image
+  renderImages(json.results);
+}
+
+// use the response from unsplash to change the image on the page
+function renderImages(data) {
+  // remove old images
+  document.getElementById("images").innerHTML = "";
+
+  // loop through my results and render an image for each item
+  data.forEach(function (imageObj) {
+    // create a new img tag
+    const img = document.createElement("img");
+
+    // set the src and all of my new img tag
+    img.src = imageObj.urls.full;
+    img.alt = imageObj.alt_description;
+
+    // append my image to the page
+    document.getElementById("images").appendChild(img);
+  });
+}
+
+// take user input to affect the unsplash query
+const form = document.getElementById("searchForm");
+
+form.addEventListener("submit", function (event) {
+  // the form is automatically passed the event as a param, which gives us access to:
+  event.preventDefault();
+
+  const myQuery = event.target.myQuery.value;
+  getImages(myQuery);
+});
+
+getImages("korea");
