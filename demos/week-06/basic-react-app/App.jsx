83c72478220ff343896cece47692ebddb16a9809
Tim <tim.smith2814@gmail.com>
2024-01-08 09:37:26 +0000

    Add first react demo

---
+++ demos/week-06/basic-react-app/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  // the logic for a cookie counter
+  return (
+    <div>
+      <p>This is amazing! Absolute magic!</p>
+    </div>
+  );
+}
