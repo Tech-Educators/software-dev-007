05350e4dc57436820384e371fecba1dde9328078
Tim <tim.smith2814@gmail.com>
2024-02-07 11:25:35 +0000

    Add challeneg demo

---
+++ demos/week-10/rock-paper-scissors-sudo-code/app.js
@@ -0,0 +1,6 @@
+// for player 1 the keys a,s & d would represent rock paper & scissors
+// for player 2 the keys j,k & l would represent rock paper & scissors
+
+// when a player clicks one of the keys, listen to the event, and update a global variable showing that players choice
+
+// when both players have selected a key, then show the iamges and who has won
